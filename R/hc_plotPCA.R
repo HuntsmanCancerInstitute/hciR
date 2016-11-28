0e565548bd2a15fd91b6480a0e9da5bbd385ef02
Chris Stubben <chris.stubben@gmail.com>
2016-11-28 13:21:53 -0700

    add hc_plotPCA

--- R/hc_plotPCA.R
+++ R/hc_plotPCA.R
@@ -20,14 +20,13 @@
 #' colData(rld)$sample <- 1:12
 #' colData(rld)$gender <- sample(c("M", "F"), 12, TRUE)
 #' hc_plotPCA(rld, tooltip=c("sample", "gender"))
-#' }
 #' @export
 
 hc_plotPCA <- function(object, intgroup="condition", tooltip, ntop = 500){
    n  <- apply(assay(rld), 1, var)
    x  <-  head(assay(rld)[ order(n, decreasing=TRUE),], ntop)
-   pca <- prcomp(t( x ))
-   percentVar <- round(pca$sdev^2/sum(pca$sdev^2)*100, 1)
+   pca <- prcomp(t(x))
+   percentVar <- round(pca$sdev^2/sum(pca$sdev^2) * 100, 1)
 
    if(!all(intgroup %in% names(colData(object)))) stop("intgroup should match columns of colData(object)")
    group <-  apply( as.data.frame(colData(object)[, intgroup, drop=FALSE]), 1, paste, collapse=": ")
@@ -42,11 +41,10 @@ hc_plotPCA <- function(object, intgroup="condition", tooltip, ntop = 500){
       ##  "'ID: ' + this.point.ID + '<br>patient: ' + this.point.patient + '<br>gender: ' + this.point.gender
       tooltipJS <-  paste0("'", paste( tooltip, ": ' + this.point.", tooltip, sep="", collapse = " + '<br>"))
    }
-
    hchart(d, "scatter", x=PC1, y=PC2, group=INTGRP ) %>%
     hc_tooltip(formatter = JS( paste0("function(){ return (", tooltipJS, ")}"))) %>%
      hc_xAxis(title = list(text = paste0("PC1: ", percentVar[1], "% variance")),
-          gridLineWidth=1, tickLength=0, startOnTick="true", endOnTick="true") %>%
+             gridLineWidth=1, tickLength=0, startOnTick="true", endOnTick="true") %>%
       hc_yAxis(title = list(text = paste0("PC2: ", percentVar[2], "% variance"))) %>%
        hc_chart(zoomType = "xy")  %>%
         hc_exporting(enabled=TRUE, filename = "pca")
