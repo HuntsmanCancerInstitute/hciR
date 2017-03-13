92ffb02f8d03c3117297cdf0c08506fc4a2b856b
Chris Stubben <chris.stubben@gmail.com>
2017-03-12 23:27:30 -0600

    add as_matrix

---
+++ R/as_matrix.R
@@ -0,0 +1,24 @@
+#' Convert tibbles to a matrix
+#'
+#' Mainly used to convert count tables to matrix for Bioconductor functions that
+#' require a matrix as input.
+#'
+#' @param x a tibble with identifiers in column 1 (assigned as rownames) and
+#'  numbers in remaining columns
+#' @return A matrix
+#' @author Chris Stubben
+#'
+#' @examples
+#' \dontrun{
+#'   rlog(as_matrix(count_tbl))
+#' }
+#' @export
+
+
+as_matrix <- function(x, ...){
+   if(!is_tibble(x) ) stop("x must be a tibble")
+   
+  y <- as.matrix.data.frame(x[,-1])
+  rownames(y) <- x[[1]]
+  y
+}
