7e1b2c47fcc54beee0fd58a3f667c591a7d88abb
Chris Stubben <chris.stubben@gmail.com>
2018-11-12 13:26:40 -0700

    update plot_interactions

--- R/plot_interactions.R
+++ R/plot_interactions.R
@@ -4,6 +4,8 @@
 #' @param intgroups two column names in \code{attr(x, "colData")} to use for grouping
 #' @param ylab y-axis label
 #' @param scaled scale counts
+#' @param se display confidence interval, default FALSE
+#' @param lwd smooth line width
 #' @param n total number of genes to plot
 #' @param reorder level names to reorder factor levels on x-axis
 #'
@@ -14,11 +16,11 @@
 #' @examples
 #' \dontrun{
 #'  x <- top_counts( res, rld, top=25)
-#'  plot_interactions(x,  c("time", "trt"), ylab = "scaled rlog")
+#'  plot_interactions(x,  c("time", "trt"))
 #' }
 #' @export
 
-plot_interactions <- function(x, intgroups, ylab = "count", scaled = TRUE, n=40, reorder){
+plot_interactions <- function(x, intgroups, ylab = "scaled rlog", scaled = TRUE, se = FALSE, lwd = 0.5, n=40, reorder){
    if(length(intgroups) != 2) stop( "intgroups should be a vector of two column names")
    if(!tibble::is_tibble(x)) stop("x should be a tibble from top_counts")
    s1  <- attr(x, "colData")
@@ -30,18 +32,15 @@ plot_interactions <- function(x, intgroups, ylab = "count", scaled = TRUE, n=40,
       message("Displaying top ", n, " genes from top_counts")
        x1 <- x1[, 1:n]
    }
-   x1 <- data.frame( x1)
-   y <- split(x1,  s1[, intgroups] )
-   y <- lapply(y, colMeans, na.rm=TRUE)
-   z <- dplyr::bind_rows( lapply(y, tibble::as_tibble), .id="stage") %>%
-     tidyr::separate(stage, into= intgroups, sep="\\.")
+
+   y <- data.frame(s1[, intgroup], x1)
+   z <- tidyr::gather(y, -intgroup, key="gene", value = "rlog")
     ## reorder?
     if(!missing(reorder)) z[[1]] <- factor(z[[1]], levels = reorder)
-   z <-    dplyr::mutate(z, gene= rep( names(y[[1]]),  length(y) )) %>%
-      filter(!is.na(value))
-   ggplot2::ggplot(z, ggplot2::aes_string(x = intgroups[1], y = "value",
+  ## add option to drop geom_poins (only fitted line)
+   ggplot2::ggplot(z, ggplot2::aes_string(x = intgroups[1], y = "rlog",
       group =intgroups[2], shape =intgroups[2], color=intgroups[2])) +
-       ggplot2::geom_line() +  ggplot2::geom_point() +
+       ggplot2::geom_smooth(method="lm", se=se, lwd=0.5)  +  ggplot2::geom_point() +
         ggplot2::facet_wrap(~gene) + ggplot2::ylab( ylab)
 
 }
